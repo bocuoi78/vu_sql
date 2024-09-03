4d7c1815b9c4e2215965b4f9f38994000053d84b
Nguyễn Duy Khánh <136660702+bocuoi78@users.noreply.github.com>
2024-09-03 16:17:14 +0700

    Add files via upload

---
+++ uniform_data.sql
@@ -0,0 +1,19 @@
+/*
+-- Query: SELECT * FROM student_db.uniform
+LIMIT 0, 1000
+
+-- Date: 2024-09-03 16:16
+*/
+INSERT INTO `` (`id`,`gender`,`quantity_in_stock`,`size_code`,`size_name`) VALUES ('02ff1dd8-8273-4926-9603-cec2f853ea8c','0',10,5,'XXL');
+INSERT INTO `` (`id`,`gender`,`quantity_in_stock`,`size_code`,`size_name`) VALUES ('178fb35a-c6e7-4fc2-9df0-4788408b81e0','0',10,4,'XL');
+INSERT INTO `` (`id`,`gender`,`quantity_in_stock`,`size_code`,`size_name`) VALUES ('5f0f2e27-7f34-4a03-922b-1c348c5f6e3f','1',10,6,'XXXL');
+INSERT INTO `` (`id`,`gender`,`quantity_in_stock`,`size_code`,`size_name`) VALUES ('7dbb7e75-1fe0-45cd-8746-f001899d9b8c','1',10,5,'XXL');
+INSERT INTO `` (`id`,`gender`,`quantity_in_stock`,`size_code`,`size_name`) VALUES ('82ce6ab7-5ee4-4271-bc49-7efa7b416e61','0',300,3,'L');
+INSERT INTO `` (`id`,`gender`,`quantity_in_stock`,`size_code`,`size_name`) VALUES ('b6aabe4a-4792-40fa-9541-4219c63472e4','1',10,2,'M');
+INSERT INTO `` (`id`,`gender`,`quantity_in_stock`,`size_code`,`size_name`) VALUES ('caffb992-805d-4b22-8667-63d03cbbf74e','1',500,3,'L');
+INSERT INTO `` (`id`,`gender`,`quantity_in_stock`,`size_code`,`size_name`) VALUES ('ce0fa238-19df-4f96-a125-a0f865437c70','1',10,7,'XXXXL');
+INSERT INTO `` (`id`,`gender`,`quantity_in_stock`,`size_code`,`size_name`) VALUES ('cfda6a9e-3d01-402f-8cb3-8c6f8aedf45a','0',10,1,'S');
+INSERT INTO `` (`id`,`gender`,`quantity_in_stock`,`size_code`,`size_name`) VALUES ('e6b9a6c8-c391-4e34-ac27-af0c9b0b9b38','1',10,1,'S');
+INSERT INTO `` (`id`,`gender`,`quantity_in_stock`,`size_code`,`size_name`) VALUES ('e939a270-9022-4b86-a59e-45945a321620','0',10,6,'XXXL');
+INSERT INTO `` (`id`,`gender`,`quantity_in_stock`,`size_code`,`size_name`) VALUES ('ec1f765d-581c-44e7-8f7c-34d6d045a532','1',10,4,'XL');
+INSERT INTO `` (`id`,`gender`,`quantity_in_stock`,`size_code`,`size_name`) VALUES ('fd84b59b-f7f3-42ac-bce2-b75d6507008b','0',10,2,'M');
